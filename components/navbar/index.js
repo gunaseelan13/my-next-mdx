14434808feee93c71b7beee3b36639350cab0a10
gunaseelan13 <guna.nehru.13@gmail.com>
2021-05-15 11:08:53 +0530

    Dependencies change

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import {Nav,NavbarContainer,NavLogo, NavMenu,NavItems,NavLinks,} from "./NavElements";
+import {Nav,NavbarContainer,NavLogo, NavMenu,NavItems,NavLinks} from "./NavElements";
   
 const Navbar = () => {
     return (
