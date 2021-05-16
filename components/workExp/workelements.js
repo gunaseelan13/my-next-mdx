b0f56c6fa23979e0c510c36d1ccee9bbe72aaed0
gunaseelan13 <guna.nehru.13@gmail.com>
2021-05-16 23:29:26 +0530

    image change

--- components/workExp/workelements.js
+++ components/workExp/workelements.js
@@ -7,7 +7,7 @@ export const FeaturedContainer = styled.div`
     // padding: 20px 10px;
     
     margin: 0px 20px 0px 20px;
-    grid-template-columns: repeat(auto-fit,minmax(400px,1fr));
+    grid-template-columns: repeat(auto-fit,minmax(300px,480px));
     grid-gap: 0px 30px;
     
     // @media screen and (max-width: 768px) {
